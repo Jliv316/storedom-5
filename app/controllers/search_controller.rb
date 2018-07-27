cf78f4f53abb95e84811bc1e9d0ee20c8e02620f
Jliv316 <jclivingston316@gmail.com>
2018-07-27 11:48:43 -0600

    pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
+require 'will_paginate/array'
 class SearchController < ApplicationController
   def index
-    @stores = StorePresenter.new(params[:zip]).stores
+  stores = StorePresenter.new(params[:zip]).stores
+  @stores = stores.paginate(page: params[:page], per_page: 10)
   end
 end
--- spec/features/user_can_find_stores_by_zip_spec.rb
+++ spec/features/user_can_find_stores_by_zip_spec.rb
@@ -21,5 +21,25 @@ describe "User can visit root page" do
       expect(page).to have_content("Phone Number: 303-742-8039")
       expect(page).to have_content("Store Type: Big Box")
     end
+
+    it "should only display 10 of the 14 on the first page" do
+      json_response = File.open("./fixtures/stores_by_zip.json")
+      url = "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=city,longName,distance,phone,storeType&pageSize=14&apiKey=tq72hpvtbbd4mue6f23kmsw2"
+      
+      stub_request(:get, url).to_return(status: 200, body: json_response)
+      
+      visit(root_path)
+
+      fill_in id="zip", with: 80202
+      click_on "Search"
+
+      expect(current_path).to eq("/search")
+      expect(page).to have_content("14 total stores")
+      expect(page).to have_content("Name: Belmar")
+      expect(page).to have_content("City: Lakewood")
+      expect(page).to have_content("Distance: 5.31")
+      expect(page).to have_content("Phone Number: 303-742-8039")
+      expect(page).to have_content("Store Type: Big Box")
+    end
   end
 end
