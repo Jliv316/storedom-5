7b4e012ed62a15b963442b1311838ae583861004
Jliv316 <jclivingston316@gmail.com>
2018-07-27 12:05:21 -0600

    finishes 3 users stories

--- spec/features/user_can_find_stores_by_zip_spec.rb
+++ spec/features/user_can_find_stores_by_zip_spec.rb
@@ -36,5 +36,24 @@ describe "User can visit root page" do
       expect(current_path).to eq("/search")
       expect(page).to have_content("10 of 14 stores displayed")
     end
+
+    it "should see pagination links" do
+      json_response = File.open("./fixtures/stores_by_zip.json")
+      url = "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=city,longName,distance,phone,storeType&pageSize=14&apiKey=tq72hpvtbbd4mue6f23kmsw2"
+      
+      stub_request(:get, url).to_return(status: 200, body: json_response)
+      
+      visit(root_path)
+
+      fill_in id="zip", with: 80202
+      click_on "Search"
+
+      expect(current_path).to eq("/search")
+      expect(page).to have_content("10 of 14 stores displayed")
+
+      click_on '2'
+
+      expect(page).to have_content("4 of 14 stores displayed")
+    end
   end
 end
